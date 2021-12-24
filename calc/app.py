d34c3705bced04bf6f39de2f86767fc3074d6544
Brent Jameson <brentjameson@Brents-MBP.attlocal.net>
2021-12-24 12:15:31 -0500

    completed exercise

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,42 @@
-# Put your app in here.
+from flask import Flask, request
+import operations
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_nums():
+    '''Returns a + b'''
+    a = request.args.get('a', type = int)
+    b = request.args.get('b', type = int)
+    res = str(operations.add(a,b))
+    html = f'<html><body><h1>{res}</h1></body></html>'
+    return html
+
+@app.route('/sub')
+def sub_nums():
+    '''Returns a - b'''
+    a = request.args.get('a', type = int)
+    b = request.args.get('b', type = int)
+    res = str(operations.sub(a,b))
+    html = f'<html><body><h1>{res}</h1></body></html>'
+    return html
+
+@app.route('/mult')
+def mult_nums():
+    '''Returns a * b'''
+    a = request.args.get('a', type = int)
+    b = request.args.get('b', type = int)
+    res = str(operations.mult(a,b))
+    html = f'<html><body><h1>{res}</h1></body></html>'
+    return html
+
+@app.route('/div')
+def div_nums():
+    '''Returns a / b'''
+    a = request.args.get('a', type = int)
+    b = request.args.get('b', type = int)
+    res = str(operations.div(a,b))
+    html = f'<html><body><h1>{res}</h1></body></html>'
+    return html
+
+
